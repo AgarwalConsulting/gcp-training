21c1fc9b1720509983212cdb6352d4f8740eea7a
Gaurav Agarwal <algogrit@gmail.com>
2021-09-13 11:34:30 +0530

    Adding comments for reading and writing a file.

---
+++ challenges/gcf/02-file-encryption.md
@@ -0,0 +1,9 @@
+# File Encryption
+
+- Listen for any new files uploaded into your chosen bucket
+
+- Perform a [rot13 substitution](https://rot13.com/) of the characters in the file
+
+  - Optional: Make the rot13 substitution in a separate function
+
+- Save the file into the same bucket under a different directory
--- examples/cloudfunctions/java/03-background/01-gcs/src/main/java/com/algogrit/functions/SimpleGCS.java
+++ examples/cloudfunctions/java/03-background/01-gcs/src/main/java/com/algogrit/functions/SimpleGCS.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,11 +32,39 @@ import java.util.logging.Logger;
 
 public class SimpleGCS implements BackgroundFunction<GCSEvent> {
   private static final Logger logger = Logger.getLogger(SimpleGCS.class.getName());
+  // private static Storage storage = StorageOptions.getDefaultInstance().getService();
 
   @Override
   public void accept(GCSEvent event, Context context) {
     logger.info("Processing file: " + event.name);
     // throw new UnsupportedOperationException("Not supported yet.");
+
+    //// Read file from GCS Bucket
+    // String BUCKET_NAME = System.getenv("BUCKET_NAME");
+    // Blob blob = storage.get(BUCKET_NAME, event.name);
+    // try (ReadChannel reader = blob.reader()) {
+    //   ByteBuffer bytes = ByteBuffer.allocate(64 * 1024);
+    //   StringBuilder buffer = new StringBuilder();
+    //    int numCharsRead;
+    //   while ((numCharsRead = reader.read(bytes)) > 0) {
+    //    bytes.flip();
+    //    // Content credits: https://www.baeldung.com/java-convert-reader-to-string
+    //    // do something with bytes; currently reading into a buffer
+    //    buffer.append(bytes, 0, numCharsRead);
+    //    bytes.clear();
+    //   }
+    // String targetString = buffer.toString();
+    //  }
+
+    //// Write file to GCS bucket
+    // Create a bucket - Optional
+    // String bucketName = "my_unique_bucket"; // Change this to something unique
+    // Bucket bucket = storage.create(BucketInfo.of(bucketName));
+
+    // // Upload a blob to the newly created bucket
+    // BlobId blobId = BlobId.of(bucketName, "my_blob_name");
+    // BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+    // Blob blob = storage.create(blobInfo, "a simple blob".getBytes(UTF_8));
   }
 
   public static class GCSEvent {
