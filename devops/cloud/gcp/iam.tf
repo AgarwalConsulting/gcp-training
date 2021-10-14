bf8532d226daff5b67d65b7c3aeabb8a756ee469
Gaurav Agarwal <algogrit@gmail.com>
2021-10-14 15:25:56 +0530

    Updating peermissions.

--- devops/cloud/gcp/iam.tf
+++ devops/cloud/gcp/iam.tf
@@ -1,4 +1,16 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+
+## IAM bindings
+resource "google_project_iam_binding" "cm-cloud-cli" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+
+  for_each = toset(var.users_list)
+  members  = ["user:${each.key}"]
+}
+
+## Member roles
+
 resource "google_project_iam_member" "cm-cf" {
   project = var.project_id
   role    = "roles/cloudfunctions.admin"
@@ -13,20 +25,12 @@ resource "google_project_iam_member" "cm-gcs" {
   member  = "user:${each.key}"
 }
 
-resource "google_project_iam_binding" "cm-pubsub" {
+resource "google_project_iam_member" "cm-pubsub" {
   project = var.project_id
   role    = "roles/pubsub.admin"
-
   for_each = toset(var.users_list)
-  members  = ["user:${each.key}"]
+  member  = "user:${each.key}"
 }
-
-# resource "google_project_iam_member" "cm-pse" {
-#   project = var.project_id
-#   role    = "roles/pubsub.editor"
-#   for_each = toset(var.users_list)
-#   member  = "user:${each.key}"
-# }
 
 resource "google_project_iam_member" "cm-logva" {
   project = var.project_id
@@ -42,42 +46,41 @@ resource "google_project_iam_member" "cm-monitorv" {
   member  = "user:${each.key}"
 }
 
-resource "google_project_iam_binding" "cm-cloud-cli" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
+# resource "google_project_iam_member" "cm-pse" {
+#   project = var.project_id
+#   role    = "roles/pubsub.editor"
+#   for_each = toset(var.users_list)
+#   member  = "user:${each.key}"
+# }
 
-  for_each = toset(var.users_list)
-  members  = ["user:${each.key}"]
-}
-
-# resource "google_project_iam_binding" "cm-bigquery" {
+# resource "google_project_iam_member" "cm-bigquery" {
 #   project = var.project_id
 #   role    = "roles/bigquery.admin"
 
 #   for_each = toset(var.users_list)
-#   members  = ["user:${each.key}"]
+#   member  = "user:${each.key}"
 # }
 
-# resource "google_project_iam_binding" "cm-bigtable" {
+# resource "google_project_iam_member" "cm-bigtable" {
 #   project = var.project_id
 #   role    = "roles/bigtable.admin"
 
 #   for_each = toset(var.users_list)
-#   members  = ["user:${each.key}"]
+#   member  = "user:${each.key}"
 # }
 
-# resource "google_project_iam_binding" "cm-spanner" {
+# resource "google_project_iam_member" "cm-spanner" {
 #   project = var.project_id
 #   role    = "roles/spanner.admin"
 
 #   for_each = toset(var.users_list)
-#   members  = ["user:${each.key}"]
+#   member  = "user:${each.key}"
 # }
 
-# resource "google_project_iam_binding" "cm-cloudsql" {
+# resource "google_project_iam_member" "cm-cloudsql" {
 #   project = var.project_id
 #   role    = "roles/cloudsql.admin"
 
 #   for_each = toset(var.users_list)
-#   members  = ["user:${each.key}"]
+#   member  = "user:${each.key}"
 # }
